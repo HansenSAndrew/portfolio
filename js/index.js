7f1221b209f9f84e285754bbce98133107fe1de3
Andrew Hansen <123025570+HansenSAndrew@users.noreply.github.com>
2024-03-26 17:02:47 -0400

    Used Babel ES6 -> ES5 to Fix Mobile Use

--- js/index.js
+++ js/index.js
@@ -1,25 +1,30 @@
-import data from './portfolio.json' assert {type: 'json'}
+var _process$versions, _import$meta$resolve, _import$meta$resolve2, _import$meta;
+const data = await (typeof process === "object" && 
+(_process$versions = process.versions) !== null &&
+ _process$versions !== void 0 && _process$versions.node ? Promise.all([import("fs"),
+  import("module")]).then(([fs, module]) => new
+   Promise((a => (r, j) => fs.readFile(a, (e, d) => e
+    ? j(e) : r(d)))(module.createRequire(import.meta.url)
+    .resolve('./portfolio.json')))).then(JSON.parse) : fetch((_import$meta$resolve = (_import$meta$resolve2 = (_import$meta = import.meta).resolve) === null 
+    || _import$meta$resolve2 === void 0 ? void 0 : _import$meta$resolve2.call(_import$meta, './portfolio.json')) !== null &&
+     _import$meta$resolve !== void 0 ? _import$meta$resolve : new URL('./portfolio.json', import.meta.url)).then(r => r.json()));
 
-// const body = document.body;
-const body = document.body
+const body = document.body;
 
 // Creates Div for the 'cards'
 const div = document.createElement('div');
 div.className = "Projects";
-
 body.appendChild(div);
-
 
 // Loops through portfolio.json and creates 'Cards.'
 for (var i in data.projects) {
-    var name = data.projects[i].name;
-    var description = data.projects[i].description;
-    var repository = data.projects[i].repository;
-    var picture = data.projects[i].picture;
-
-    var card = document.createElement('div');
-    card.className = "Card";
-    card.innerHTML = `
+  var name = data.projects[i].name;
+  var description = data.projects[i].description;
+  var repository = data.projects[i].repository;
+  var picture = data.projects[i].picture;
+  var card = document.createElement('div');
+  card.className = "Card";
+  card.innerHTML = `
     <img src=${picture} alt="Picture from Project">
     <span>
         <h1>
@@ -30,8 +35,6 @@ for (var i in data.projects) {
         <p>${description}</p>
     </span>
     `;
-
-    div.appendChild(card);
+  div.appendChild(card);
 }
-
 console.log(data);
